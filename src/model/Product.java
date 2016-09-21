a74933d0ca9aa826503a10289dcd02e5aba1f8ad
hein.dehaan <hein.dehaan@laptop-Hein.getthere.tld>
2016-09-21 13:13:16 +0200

    Made switch from multiple ifs. ALWAYS DO THIS. Much more efficient.

--- src/model/Product.java
+++ src/model/Product.java
@@ -6,21 +6,32 @@ abstract public class Product {
 	static final int liter = 3;
 	static final int stuk = 4;
 	static final int pak = 5;
-	
-	public Product(String naam, int prijsPerEenheid, String imageUrl, int eenheid){
+
+	public Product(String naam, int prijsPerEenheid, String imageUrl, int eenheid) {
 	}
-	public Product(String naam, int prijsPerEenheid, String imageUrl){
+
+	public Product(String naam, int prijsPerEenheid, String imageUrl) {
 		this(naam, prijsPerEenheid, imageUrl, 1);
 	}
-	public static String getEenheidNaam(int eenheid){
-		String eenheidString = "";
-		if(eenheid == Product.gram) eenheidString = "gram";
-		if(eenheid == Product.kilo) eenheidString = "kilo";
-		if(eenheid == Product.liter) eenheidString = "liter";
-		if(eenheid == Product.stuk) eenheidString = "stuk";
-		if(eenheid == Product.pak) eenheidString = "pak";
-		return eenheidString;
+
+	public static String getEenheidNaam(int eenheid) {
+		switch (eenheid) {
+			case gram:
+				return "gram";
+			case kilo:
+				return "kilo";
+			case liter:
+				return "liter";
+			case stuk:
+				return "stuk";
+			case pak:
+				return "pak";
+			default:
+				return "";
+		}
 	}
+
 	abstract public String toString();
+
 	abstract public int getPrijs(int hoeveelheid);
 }
